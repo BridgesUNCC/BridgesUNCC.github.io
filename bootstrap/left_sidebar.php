76a79f848a6f47aa7fef1e97a25164c10c48894a
Kalpathi Subramanian <krs@uncc.edu>
2016-07-23 23:23:32 -0400

    site update

--- bootstrap/left_sidebar.php
+++
@@ -1,8 +0,0 @@
-<?php
-<div class="col-md-2 sidenav">
-echo 
-     '<p><a href="https://bridgesuncc.github.io/index.html">Home</a></p>
-      <p><a href="#">Contact Us</a></p>
-      <p><a href="#">BRIDGES Team</a></p>';
-?>
-</div>
