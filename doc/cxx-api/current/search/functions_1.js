aee2cf5089524295bc7f82574c64d34f327cf88a
Kalpathi Subramanian <krs@uncc.edu>
2016-08-05 17:17:49 -0400

    Updating Bridges Docs

--- doc/cxx-api/current/search/functions_1.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['bintreeelement',['BinTreeElement',['../classbridges_1_1_bin_tree_element.html#a1c60db90bda9ecd3f5a61b5f33f49173',1,'bridges::BinTreeElement::BinTreeElement(BinTreeElement *l, BinTreeElement *r, const E &amp;e=E(), const string &amp;lab=string())'],['../classbridges_1_1_bin_tree_element.html#a37d12669e5bfe13ebf230dd8fd2a5816',1,'bridges::BinTreeElement::BinTreeElement(const E &amp;e=E(), const string &amp;lab=string())']]],
-  ['bstelement',['BSTElement',['../classbridges_1_1_b_s_t_element.html#aff7dbbb4011e85ea492d9a0c921895c5',1,'bridges::BSTElement::BSTElement(const K &amp;k, BSTElement *l, BSTElement *r, const E &amp;val=E(), const string &amp;lab=string())'],['../classbridges_1_1_b_s_t_element.html#a13d32741606a8e3375b39f6dde99da5b',1,'bridges::BSTElement::BSTElement(const K &amp;k, const E &amp;val=E(), const string &amp;lab=string())']]]
-];
--- doc/java-api/current/search/functions_2.js
+++
@@ -1,8 +0,0 @@
-var searchData=
-[
-  ['close',['close',['../classbridges_1_1validation_1_1_output_log.html#a6ab7881cd35aa11cc830dab4732fe66d',1,'bridges::validation::OutputLog']]],
-  ['color',['Color',['../classbridges_1_1base_1_1_color.html#ab6d71ac2ee1430fb2db2fbe34e692de8',1,'bridges.base.Color.Color()'],['../classbridges_1_1base_1_1_color.html#a8a8b586ef5d11853c7abb82ed002f522',1,'bridges.base.Color.Color(int r, int g, int b, int a)'],['../classbridges_1_1base_1_1_color.html#a5fab564fa4eec8bece64f847ebd42948',1,'bridges.base.Color.Color(int r, int g, int b)']]],
-  ['compareto',['compareTo',['../classbridges_1_1base_1_1_element.html#a6cd4c4f15c6a4f87f59e443cffe87a20',1,'bridges.base.Element.compareTo()'],['../classbridges_1_1data__src__dependent_1_1_earthquake_tweet.html#a6b4de2a600b93ed96f6cde8b719ea645',1,'bridges.data_src_dependent.EarthquakeTweet.compareTo()'],['../classbridges_1_1data__src__dependent_1_1_earthquake_u_s_g_s.html#a60cad0a286825f77cd2900265acae982',1,'bridges.data_src_dependent.EarthquakeUSGS.compareTo()']]],
-  ['connector',['Connector',['../classbridges_1_1connect_1_1_connector.html#a167800699b2d191bd625d9c8c8cd9e6f',1,'bridges::connect::Connector']]],
-  ['converttweet',['convertTweet',['../classbridges_1_1connect_1_1_data_formatter.html#ad87ca06456fa4dc110e167e84f2d4447',1,'bridges::connect::DataFormatter']]]
-];
