f7e76c1648abdbb2736f89f89a6bbc2872e03607
Kalpathi Subramanian <krs@uncc.edu>
2018-03-04 00:49:04 -0500

    more updates..

---
+++ Hello_World_Tutorials/tutorial-source-code/Tree/avl.cpp
@@ -0,0 +1,58 @@
+#include &lt;string&gt;
+#include "Bridges.h"
+#include "AVLTreeElement.h"
+
+
+using namespace std;
+using namespace bridges;
+
+int main() {
+
+
+	Bridges::initialize(11, "YOUR_USER_ID", "YOUR_API_KEY"); 
+
+	AVLTreeElement&lt;int, string&gt; *tle0 = 
+				new AVLTreeElement&lt;int, string&gt;(10, "10", "10");
+	AVLTreeElement&lt;int, string&gt; *tle1 = 
+				new AVLTreeElement&lt;int, string&gt;(5, "5", "5");
+	AVLTreeElement&lt;int, string&gt; *tle2 = 
+				new AVLTreeElement&lt;int, string&gt;(3, "3", "3");
+	AVLTreeElement&lt;int, string&gt; *tle3=  
+				new AVLTreeElement&lt;int, string&gt;(20, "20","20");
+
+					 //  link the nodes
+	tle0->setLeft(tle1);
+	tle0->setRight(tle3);
+	tle1->setLeft(tle2);
+
+					// set attributes
+	tle0->getVisualizer()->setColor(Color("red"));
+	tle1->getVisualizer()->setColor(Color("green"));
+	tle2->getVisualizer()->setColor(Color("blue"));
+	tle3->getVisualizer()->setColor(Color("yellow"));
+
+	tle0->getLinkVisualizer(tle1)->setColor(Color("red"));
+	tle1->getLinkVisualizer(tle2)->setColor(Color("green"));
+	tle0->getLinkVisualizer(tle3)->setColor(Color("blue"));
+
+					// illustrate balance factors in the node labels
+	tle0->setBalanceFactor(-1);
+	tle1->setBalanceFactor(-1);
+	tle2->setBalanceFactor(0);
+	tle3->setBalanceFactor(0);
+
+					// display the balance factors (on mouseover)
+	tle0->setLabel(std::to_string(tle0->getBalanceFactor()));
+	tle1->setLabel(std::to_string(tle1->getBalanceFactor()));
+	tle2->setLabel(std::to_string(tle2->getBalanceFactor()));
+	tle3->setLabel(std::to_string(tle3->getBalanceFactor()));
+
+	Bridges::setTitle("AVL Tree Example");
+					// provide BRIDGES the  handle to the tree structure
+	Bridges::setDataStructure(tle0);
+
+
+	Bridges::visualize();
+
+	return 0;
+}
---
+++ Hello_World_Tutorials/tutorial-source-code/Tree/bst_eq.java
@@ -0,0 +1,53 @@
+import bridges.connect.Bridges;
+import bridges.base.BSTElement;
+import bridges.data_src_dependent.EarthquakeUSGS;
+import bridges.data_src_dependent.Tweet;
+import bridges.data_src_dependent.USGSaccount;
+import java.util.List;
+
+public class bst_eq {
+	public static void main(String[] args) throws Exception{
+    	
+   					//create the Bridges object
+		Bridges bridges = new Bridges(10, "YOUR_API_KEY", "YOUR_USER_ID");
+
+		bridges.setTitle("A Binary Search Tree Example with Earthquake Data");
+
+				// Retrieve a list of 10 earthquake  records  from 
+				// USGS using the BRIDGES API
+		USGSaccount name = new USGSaccount( "earthquake" );
+		List&lt;EarthquakeUSGS&gt; eqlist = Bridges.getEarthquakeUSGSData(name, 10);
+
+					// create BST nodes and insert into a tree
+		BSTElement&lt;Double, EarthquakeUSGS&gt; root = null; 
+		for (int k = 0; k < 10; k++) {
+			BSTElement&lt;Double, EarthquakeUSGS&gt; bst_node = 
+				new BSTElement&lt;Double, EarthquakeUSGS&gt;(eqlist.get(k).getMagnitude(), eqlist.get(k)); 
+					// set label of the node
+			bst_node.setLabel(eqlist.get(k).getTitle() + "\n\nLat/Long: ( " + 
+				eqlist.get(k).getLatit() + "," + eqlist.get(k).getLongit() + " )\n\n" + 
+				eqlist.get(k).getTime());
+			root = insert_R (root, bst_node);
+		}
+
+
+								// set some visual attributes
+		root.getVisualizer().setColor("red");
+
+             					//set visualizer type
+		bridges.setDataStructure(root);
+        						// visualize the tree
+		bridges.visualize();
+    }
+				// recursive insert method to insert nodes into a binary search tree
+	public static BSTElement&lt;Double, EarthquakeUSGS&gt; insert_R (BSTElement&lt;Double, EarthquakeUSGS&gt; rt, 
+					BSTElement&lt;Double, EarthquakeUSGS&gt; new_el) {
+		if (rt == null)
+			return new_el;
+		else if (new_el.getKey() < rt.getKey())
+			 rt.setLeft(insert_R(rt.getLeft(), new_el));
+		else 
+			 rt.setRight(insert_R(rt.getRight(), new_el));
+		return rt;
+	}
+}
