df2cc1dd2e3a004980669e05764b1eb62b2a2573
Kalpathi Subramanian <krs@uncc.edu>
2018-03-04 01:34:02 -0500

    more updates..

---
+++ Hello_World_Tutorials/tutorial-source-code/List/circ_sllist.cpp
@@ -0,0 +1,100 @@
+/**
+ * Created by Kalpathi Subramanian, 1/30/18
+ * krs@uncc.edu
+ */
+
+#include "Bridges.h"
+#include "CircSLelement.h"
+#include "StudentInfo.h"
+
+using namespace bridges;
+
+int main () {
+
+						// note: you must fill in with your Bridges credentials
+	Bridges::initialize(6, "YOUR_USER_ID", "YOUR_API_KEY");
+
+         					// create the linked list elements with 
+							// student data 
+	CircSLelement&lt;StudentInfo&gt; *students[] = {
+		new CircSLelement&lt;StudentInfo&gt;(
+			StudentInfo(
+				"00000000000",
+				"Gretel Chaney",
+				"CS",
+				"g.chaney@generated.com",
+				"magenta",
+				"blue",
+				 9.0
+			), ""),
+		new CircSLelement&lt;StudentInfo&gt;(
+			StudentInfo(
+				"00000000001",
+				"Karol Soderman",
+				"SIS",
+				"k.soderman@generated.com",
+				"magenta",
+				"red",
+				 11.0
+			), ""),
+		new CircSLelement&lt;StudentInfo&gt;(
+			StudentInfo(
+				"00000000002",
+				"Lamont Kyler",
+				"BIO",
+				"l.kyler@generated.com",
+				"yellow",
+				"green",
+				 12.0
+			), ""),
+		new CircSLelement&lt;StudentInfo&gt;(
+			StudentInfo(
+				"00000000003",
+				"Gladys Serino",
+				"CS",
+				"g.serino@generated.com",
+				"green",
+				"magenta",
+				 9.0
+			), ""),
+		new CircSLelement&lt;StudentInfo&gt;(
+			StudentInfo(
+				"00000000004",
+				"Starr Mcginn",
+				"CS",
+				"s.mcginn@generated.com",
+				"red",
+				"cyan",
+				 15.0
+			), "") 
+		};
+
+	CircSLelement&lt;StudentInfo&gt; *head =  students[0];
+	CircSLelement&lt;StudentInfo&gt; *current =  head;
+
+	int num_students = 5;
+	for(int i = 1; i < num_students; i++) {
+		current->setNext(students[i]);
+		current = current->getNext();
+	}
+						//  point the last element to the first element,
+	current->setNext(head);
+
+       					//  add visual attributes
+	current = head;
+	do {
+		current->setLabel(current->getValue().getStudentLabel());
+		current->getVisualizer()->setColor(current->getValue().getFavoriteColor());
+		
+		current->getLinkVisualizer(current->getNext())->setColor(current->getValue().getDislikeColor());
+		current->getLinkVisualizer(current->getNext())->setThickness(current->getValue().getStudentCreditHours()*0.5);
+		
+		current = current->getNext();
+	}   while(current!= head);
+
+
+						// set data structure to point to head
+	Bridges::setDataStructure(head);
+						// visualize the circular list
+	Bridges::visualize();
+}
---
+++ Hello_World_Tutorials/tutorial-source-code/List/dllist.cpp
@@ -0,0 +1,98 @@
+
+#include "Bridges.h"
+#include "DLelement.h"
+#include "StudentInfo.h"
+
+using namespace bridges;
+
+									// helper function
+
+DLelement&lt;StudentInfo&gt; *insertFront(DLelement&lt;StudentInfo&gt; *front,
+				DLelement&lt;StudentInfo&gt; *new_el);
+int main() {
+
+	Bridges::initialize(4, "YOUR_USER_ID", "YOUR_API_KEY");
+
+						// load student info
+	int num_students = 5;
+	StudentInfo students[] = {
+				StudentInfo(
+                      "00000000000",
+                      "Gretel Chaney",
+                      "CS",
+                      "g.chaney@generated.com",
+                      "magenta",
+                      "blue",
+                      9.0 
+				),
+				StudentInfo(
+                      "00000000001",
+                      "Karol Soderman",
+                      "SIS",
+                      "k.soderman@generated.com",
+                      "magenta",
+                      "red",
+                      11.0
+				),
+				StudentInfo(
+                      "00000000002",
+                      "Lamont Kyler",
+                      "BIO",
+                      "l.kyler@generated.com",
+                      "purple",
+                      "green",
+                      12.0
+				),
+				StudentInfo(
+                      "00000000003",
+                      "Gladys Serino",
+                      "CS","g.serino@generated.com",
+                      "blue",
+                      "black",
+                      9.0
+				),
+				StudentInfo("00000000004",
+                      "Starr Mcginn",
+                      "CS",
+                      "s.mcginn@generated.com",
+                      "red",
+                      "cyan",
+                      15.0)
+			};
+
+							// insert the students in front of the list
+		DLelement&lt;StudentInfo&gt; *head = nullptr;
+		for(int i = 0; i < num_students; i++){
+			head = insertFront(head, new DLelement&lt;StudentInfo&gt;(students[i], ""));
+		}
+							// add visual attributes 
+		DLelement&lt;StudentInfo&gt; *curr = head, *next;
+		while(curr != nullptr){
+			curr->setLabel(curr->getValue().getStudentLabel());
+			curr->getVisualizer()->setColor(curr->getValue().getFavoriteColor());
+
+			DLelement&lt;StudentInfo&gt; n1, n2;
+			if (curr->getNext() != nullptr) {
+				next = curr->getNext();
+				curr->getLinkVisualizer(next)->setColor(curr->getValue().getDislikeColor());
+				next->getLinkVisualizer(curr)->setColor(curr->getValue().getDislikeColor());
+			}
+			curr = curr->getNext();
+		}
+
+		Bridges::setDataStructure(head);
+		Bridges::visualize();
+
+		return 0;
+}
+
+DLelement&lt;StudentInfo&gt; *insertFront(DLelement&lt;StudentInfo&gt; *front,
+				DLelement&lt;StudentInfo&gt; *new_el){
+	if (front == nullptr)
+		return new_el;
+
+	new_el->setNext(front);
+	front->setPrev(new_el);
+	
+	return new_el;
+}
