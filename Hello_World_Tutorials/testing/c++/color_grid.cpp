ef1a3e5fcc309b3d620d91648484529dde731708
krs <krs@uncc.edu>
2018-07-11 09:31:12 -0400

    examples updates

--- Hello_World_Tutorials/testing/c++/color_grid.cpp
+++ Hello_World_Tutorials/testing/c++/color_grid.cpp
@@ -10,11 +10,14 @@ using namespace std;
 
 using namespace bridges;
 
+// This example illustrates using the Bridges color grid
+// We will build a checker grid using two different colors
+
 int main() {
-
-
 						// initialize BRIDGES
 	Bridges::initialize(14, "kalpathi60", "486749122386");
+
+						// set title for visualization
 	Bridges::setTitle("A Color Grid Example");
 
 	int width = 200, height = 200;
@@ -22,12 +25,17 @@ int main() {
 	ColorGrid *cg  = new ColorGrid(width, height, Color("red"));
 
 						// create a checkerboard pattern
+						// 10 x 10 grid
 	const int num_squares_x = 10;
 	const int num_squares_y = 10;
+
+						// compute square dimensions
 	const int sq_width = width/num_squares_x, sq_height = width/num_squares_y;
+
 	for (int j = 0; j < num_squares_y;  j++) {
 		for (int k = 0; k < num_squares_x;  k++)  {
-						// find the address of the square
+
+						// use even/odd of pixel to figure out the color of the square
 			bool x_even = (k%2) == 0;
 			bool y_even = (j%2) == 0;
 
@@ -36,10 +44,12 @@ int main() {
 				col = (x_even) ? "red" : "blue";
 			else 
 				col = (x_even) ? "blue" : "red"; 
-								// color the square
+
+						// find the address of the square
 			int origin_x = k*sq_width; 
 			int origin_y = j*sq_height;
 
+						// color the square
 			for (int y = origin_y; y < origin_y+sq_height; y++)
 			for (int x = origin_x; x < origin_x+sq_width; x++)
 				cg->set(x, y, Color(col));
