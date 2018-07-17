6b37d98ec23e915c5c42c966b864b3ab2140ae34
krs <krs@uncc.edu>
2018-07-17 08:52:56 -0400

    site docs updates

--- Hello_World_Tutorials/testing/java/to_krs.sh
+++ Hello_World_Tutorials/testing/java/to_krs.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-src_user_id="kalpathi60"
-src_api_key="486749122386"
+src_user_id="YOUR_USER_ID"
+src_api_key="YOUR_API_KEY"
 
-dest_user_id="YOUR_USER_ID"
-dest_api_key="YOUR_API_KEY"
+dest_user_id="kalpathi60"
+dest_api_key="486749122386"
 
 for i in *.java
 do
