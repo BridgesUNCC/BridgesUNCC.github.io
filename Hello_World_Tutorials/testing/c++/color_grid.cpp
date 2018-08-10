dc1b6df794e4b69444aedd015f239d0dcdc0b72b
krs <krs@uncc.edu>
2018-08-10 09:21:14 -0400

    website_tutorials minor updates

--- Hello_World_Tutorials/testing/c++/color_grid.cpp
+++ Hello_World_Tutorials/testing/c++/color_grid.cpp
@@ -12,45 +12,45 @@ using namespace bridges;
 // We will build a checker grid using two different colors
 
 int main() {
-						// initialize BRIDGES
+	// initialize BRIDGES
 	Bridges::initialize(14, "YOUR_USER_ID", "YOUR_API_KEY");
 
-						// set title for visualization
+	// set title for visualization
 	Bridges::setTitle("A Color Grid Example");
 
 	int width = 200, height = 200;
 
 	ColorGrid *cg  = new ColorGrid(width, height, Color("red"));
 
-						// create a checkerboard pattern
-						// 10 x 10 grid
+	// create a checkerboard pattern
+	// 10 x 10 grid
 	const int num_squares_x = 10;
 	const int num_squares_y = 10;
 
-						// compute square dimensions
-	const int sq_width = width/num_squares_x, sq_height = width/num_squares_y;
+	// compute square dimensions
+	const int sq_width = width / num_squares_x, sq_height = width / num_squares_y;
 
-	for (int j = 0; j < num_squares_y;  j++) 
-	for (int k = 0; k < num_squares_x;  k++) {
-						// use even/odd of pixel to figure out the color of the square
-		bool x_even = (k%2) == 0;
-		bool y_even = (j%2) == 0;
+	for (int j = 0; j < num_squares_y;  j++)
+		for (int k = 0; k < num_squares_x;  k++) {
+			// use even/odd of pixel to figure out the color of the square
+			bool x_even = (k % 2) == 0;
+			bool y_even = (j % 2) == 0;
 
-		string col; 
-		if (y_even) 
-			col = (x_even) ? "red" : "blue";
-		else 
-			col = (x_even) ? "blue" : "red"; 
+			string col;
+			if (y_even)
+				col = (x_even) ? "red" : "blue";
+			else
+				col = (x_even) ? "blue" : "red";
 
-					// find the address of the square
-		int origin_x = k*sq_width; 
-		int origin_y = j*sq_height;
+			// find the address of the square
+			int origin_x = k * sq_width;
+			int origin_y = j * sq_height;
 
-					// color the square
-		for (int y = origin_y; y < origin_y+sq_height; y++)
-		for (int x = origin_x; x < origin_x+sq_width; x++)
-			cg->set(x, y, Color(col));
-	}
+			// color the square
+			for (int y = origin_y; y < origin_y + sq_height; y++)
+				for (int x = origin_x; x < origin_x + sq_width; x++)
+					cg->set(x, y, Color(col));
+		}
 
 	Bridges::setDataStructure(cg);
 
