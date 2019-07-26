5fb11414101c80d6bd2dd96c3aa323a7508abb56
Erik Saule <esaule@uncc.edu>
2019-07-26 19:19:53 -0400

    using symlinks for doc

--- doc/java-api/current/html/search/files_9.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['label_2ejava',['Label.java',['../_label_8java.html',1,'']]],
-  ['linechart_2ejava',['LineChart.java',['../_line_chart_8java.html',1,'']]],
-  ['linkvisualizer_2ejava',['LinkVisualizer.java',['../_link_visualizer_8java.html',1,'']]]
-];
--- doc/java-api/current/html/search/variables_8.js
+++
@@ -1,9 +0,0 @@
-var searchData=
-[
-  ['i',['i',['../enumbridges_1_1base_1_1_named_symbol.html#a2cb1682196e76001eff92c3a4cea347f',1,'bridges.base.NamedSymbol.i()'],['../enumbridges_1_1base_1_1_named_symbol.html#a383613029497598192d3f874c6a38f75',1,'bridges.base.NamedSymbol.I()']]],
-  ['imdb',['imdb',['../classbridges_1_1data__src__dependent_1_1_i_m_d_b.html#a2913407abe6019a396d4a2ac086283df',1,'bridges::data_src_dependent::IMDB']]],
-  ['index',['index',['../classbridges_1_1base_1_1_array_element.html#a79c69e5046da8c297026d1e457a23182',1,'bridges::base::ArrayElement']]],
-  ['indianred',['indianred',['../enumbridges_1_1base_1_1_named_color.html#a9ff33bb61533fe661006c5716f16fb7c',1,'bridges::base::NamedColor']]],
-  ['indigo',['indigo',['../enumbridges_1_1base_1_1_named_color.html#aa4cea174d807cde767883c3624807555',1,'bridges::base::NamedColor']]],
-  ['ivory',['ivory',['../enumbridges_1_1base_1_1_named_color.html#a9425ce5f3da73a750ea6af05364c050e',1,'bridges::base::NamedColor']]]
-];
