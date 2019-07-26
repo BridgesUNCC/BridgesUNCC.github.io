90a5865fe88d947bc0a4efd617f61737487185a2
Erik Saule <esaule@uncc.edu>
2019-07-26 19:00:06 -0400

    moving to sym links

--- doc/cxx-api/current/html/search/classes_9.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['mlelement',['MLelement',['../classbridges_1_1datastructure_1_1_m_lelement.html',1,'bridges::datastructure']]],
-  ['movieactorwikidata',['MovieActorWikidata',['../classbridges_1_1dataset_1_1_movie_actor_wikidata.html',1,'bridges::dataset']]]
-];
--- doc/cxx-api/current/html/search/defines_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['circle_5fh',['CIRCLE_H',['../_circle_8h.html#a57bd15972019f454af4721c360d57b31',1,'Circle.h']]]
-];
