d9b3661acc0c25b88ae05a9c132e594e75c3d5ff
Kalpathi Subramanian <krs@uncc.edu>
2019-07-26 14:42:08 -0400

    updating codes from bridges-client-testing

---
+++ tutorials/testing/c++/song_lyrics_snippet.cpp
@@ -0,0 +1,36 @@
+#include "Bridges.h"
+#include "DataSource.h"
+#include "Array.h"
+#include "SLelement.h"
+#include "ColorGrid.h"
+#include <sstream>
+#include <string>
+
+using namespace bridges;
+
+// This program fragment illustrates how to access and retrieve the song lyrics data 
+int main(int argc, char* argv[]) {
+
+	//create the Bridges object, set credentials
+	Bridges bridges(YOUR_ASSSIGNMENT_NUMBER, "YOUR_USER_ID",
+		"YOUR_API_KEY");
+
+	// set title
+	bridges.setTitle("Accessing Song Data");
+
+	// create data source object
+	DataSource ds (&bridges);
+
+	// Get Song data 
+	Song s = ds.getSong("Harder Faster Better Stronger", "Daft Punk");
+
+	// print lyrics
+	std::cout << "Title: " << s.getSongTitle() << endl 
+			<< "Artist: " << s.getArtist() << endl 
+			<< "Album Title:" << s.getAlbumTitle() << endl 
+			<< "Released Date" << s.getReleaseDate() << endl 
+			<< "Lyrics: " << endl << endl
+			<< s.getLyrics()<<std::endl;
+
+	return 0;
+}
