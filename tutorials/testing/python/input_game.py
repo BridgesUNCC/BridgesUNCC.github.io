1f466695e06ec6af75c26852dd1d8a004f32bd55
Kalpathi Subramanian <krs@uncc.edu>
2020-01-03 21:47:26 -0500

    python tutorial updates

---
+++ tutorials/testing/python/input_game.py
@@ -0,0 +1,37 @@
+from bridges import *
+
+class InputGame (NonBlockingGame): 
+	def	 __init__(self, assid, user_id, api_key):
+		# substitute your username and api key
+		# create a game board of 10x10
+		super().__init__(assid, user_id,  api_key, 10, 10)
+
+	def	initialize(self):
+		# this gets executed once
+		for i in range(10):
+			for j in range(10):
+				self.set_bg_color(i, j, NamedColor.ivory)
+
+	def	game_loop(self): 
+		# this gets executed every frame
+		if self.key_up():
+			self.draw_symbol(1, 1, NamedSymbol.H, NamedColor.blue);
+			self.draw_symbol(1, 2, NamedSymbol.E, NamedColor.yellow);
+			self.draw_symbol(1, 3, NamedSymbol.L, NamedColor.black);
+			self.draw_symbol(1, 4, NamedSymbol.L, NamedColor.green);
+			self.draw_symbol(1, 5, NamedSymbol.O, NamedColor.red);
+		else:
+			self.draw_symbol(1, 1, NamedSymbol.none, NamedColor.blue);
+			self.draw_symbol(1, 2, NamedSymbol.none, NamedColor.yellow);
+			self.draw_symbol(1, 3, NamedSymbol.none, NamedColor.black);
+			self.draw_symbol(1, 4, NamedSymbol.none, NamedColor.green);
+			self.draw_symbol(1, 5, NamedSymbol.none, NamedColor.red);
+
+def	main():
+	game = InputGame(1, "BRIDGES_USER_ID", "BRIDGES_API_KEY")
+
+	# start the game
+	game.start()
+
+if __name__ == '__main__':
+	main()
---
+++ tutorials/testing/python/minimal_game.py
@@ -0,0 +1,23 @@
+from bridges import *
+
+class MinimalGame (NonBlockingGame): 
+	def	 __init__(self, assid, user_id, api_key):
+		# substitute your username and api key
+		# create a game board of 10x10
+		super().__init__(assid, user_id,  api_key, 10, 10)
+
+	def	initialize(self):
+		# this gets executed once
+		pass
+
+	def	game_loop(self): 
+		# this gets executed every frame
+		pass
+
+def	main():
+	game = MinimalGame(1, "BRIDGES_USER_ID", "BRIDGES_API_KEY")
+	# start the game
+	game.start()
+
+if __name__ == '__main__':
+	main()
