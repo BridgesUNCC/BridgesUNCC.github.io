1f7946ef1b8b88b15fc43380a1187e93632d92b3
Erik Saule <esaule@uncc.edu>
2020-03-06 14:51:46 -0500

    updateing python doc, there was a missing directory

--- doc/python-api/3.0.2/html/search/all_9.js
+++ doc/python-api/3.0.2/html/search/all_9.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['i',['I',['../classbridges_1_1named__symbol_1_1_named_symbol.html#a38cd2af55f5e54a95ffeb6646726f1e1',1,'bridges.named_symbol.NamedSymbol.I()'],['../classbridges_1_1named__symbol_1_1_named_symbol.html#a4d56badaec17d7ae898abeaa4f493c87',1,'bridges.named_symbol.NamedSymbol.i()']]],
-  ['identifier',['identifier',['../classbridges_1_1element_1_1_element.html#a31cdf36f75fe34aebec85b95025cc732',1,'bridges.element.Element.identifier(self)'],['../classbridges_1_1element_1_1_element.html#acdba2c20daeb16c7e6ce1f4d7192e782',1,'bridges.element.Element.identifier(self, int id)'],['../classbridges_1_1symbol_1_1_symbol.html#a84683c105c53b4a35e7444f7c2a6064d',1,'bridges.symbol.Symbol.identifier()']]],
+  ['identifier',['identifier',['../classbridges_1_1element_1_1_element.html#a8eb98e53169fc83f6adde36e1896cf2f',1,'bridges.element.Element.identifier(self)'],['../classbridges_1_1element_1_1_element.html#a70ac403466ed78146974a22cebfa0c19',1,'bridges.element.Element.identifier'],['../classbridges_1_1symbol_1_1_symbol.html#ab6701b43acd3e24077b1bc385b35fdf1',1,'bridges.symbol.Symbol.identifier()']]],
   ['ids',['ids',['../classbridges_1_1element_1_1_element.html#a61f02c915a65554b76dd6534e5a4d834',1,'bridges::element::Element']]],
   ['ih',['ih',['../classbridges_1_1non__blocking__game_1_1_non_blocking_game.html#a5fd9d6e53b58f6237372dfaad03db961',1,'bridges::non_blocking_game::NonBlockingGame']]],
   ['incache',['inCache',['../classbridges_1_1data__src__dependent_1_1lru__cache_1_1lru__cache.html#abfd53102b85993476f3dc75416811761',1,'bridges::data_src_dependent::lru_cache::lru_cache']]],
@@ -9,8 +9,8 @@ var searchData=
   ['indianred',['indianred',['../classbridges_1_1named__color_1_1_named_color.html#a06c0ed4b65a3ef82c23d36fc3157ef83',1,'bridges::named_color::NamedColor']]],
   ['indigo',['indigo',['../classbridges_1_1named__color_1_1_named_color.html#a42088966a61cfaae76239ac451185774',1,'bridges::named_color::NamedColor']]],
   ['initialize',['initialize',['../classbridges_1_1gamebase_1_1_game_base.html#a336629d190f1601f1211ca57e4d5427d',1,'bridges::gamebase::GameBase']]],
-  ['initialize_5fgame_5fgrid',['initialize_game_Grid',['../classbridges_1_1game__grid_1_1_game_grid.html#a54a27dd797ab47d03d2f98b8f3e319c1',1,'bridges::game_grid::GameGrid']]],
-  ['initialize_5fgrid',['initialize_grid',['../classbridges_1_1color__grid_1_1_color_grid.html#a7b21de607d59433f390ad4026aa9c3cf',1,'bridges::color_grid::ColorGrid']]],
+  ['initialize_5fgame_5fgrid',['initialize_game_Grid',['../classbridges_1_1game__grid_1_1_game_grid.html#aefa77d94a0f5d43d029e5bb9856dd911',1,'bridges::game_grid::GameGrid']]],
+  ['initialize_5fgrid',['initialize_grid',['../classbridges_1_1color__grid_1_1_color_grid.html#ad2b3ab19751cbf629096a25e31bb7f42',1,'bridges::color_grid::ColorGrid']]],
   ['input_5fhelper_2epy',['input_helper.py',['../input__helper_8py.html',1,'']]],
   ['inputhelper',['InputHelper',['../classbridges_1_1input__helper_1_1_input_helper.html',1,'bridges::input_helper']]],
   ['ivory',['ivory',['../classbridges_1_1named__color_1_1_named_color.html#ae3bb8f3d628376937706b7edaad02230',1,'bridges::named_color::NamedColor']]]
--- doc/python-api/3.0.2/html/search/functions_5.js
+++ doc/python-api/3.0.2/html/search/functions_5.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['edge_5fdata',['edge_data',['../classbridges_1_1edge_1_1_edge.html#abc3f52467d38ad2d7e06113d0c714ed6',1,'bridges.edge.Edge.edge_data(self)'],['../classbridges_1_1edge_1_1_edge.html#a1ce792ec5e30535e5546eb4d354245eb',1,'bridges.edge.Edge.edge_data(self, data)']]],
-  ['edges',['edges',['../classbridges_1_1data__src__dependent_1_1osm_1_1_osm_data.html#a96e7f48b55ab3fe1f2f9a6b52442d233',1,'bridges.data_src_dependent.osm.OsmData.edges(self)'],['../classbridges_1_1data__src__dependent_1_1osm_1_1_osm_data.html#a601684749b7fd04859418332f5c145f7',1,'bridges.data_src_dependent.osm.OsmData.edges(self, EdgeList edges)'],['../classbridges_1_1data__src__dependent_1_1osm_1_1_osm_data.html#a1ec1018524a570304e80fb662e266f12',1,'bridges.data_src_dependent.osm.OsmData.edges(self)']]],
+  ['edge_5fdata',['edge_data',['../classbridges_1_1edge_1_1_edge.html#abc3f52467d38ad2d7e06113d0c714ed6',1,'bridges.edge.Edge.edge_data(self)'],['../classbridges_1_1edge_1_1_edge.html#a7429980b52d754159629c73c316cbc41',1,'bridges.edge.Edge.edge_data(self, data)']]],
+  ['edges',['edges',['../classbridges_1_1data__src__dependent_1_1osm_1_1_osm_data.html#a1ec1018524a570304e80fb662e266f12',1,'bridges.data_src_dependent.osm.OsmData.edges(self)'],['../classbridges_1_1data__src__dependent_1_1osm_1_1_osm_data.html#a9a25900d86edcef59c6635ffae2dc5c3',1,'bridges.data_src_dependent.osm.OsmData.edges'],['../classbridges_1_1data__src__dependent_1_1osm_1_1_osm_data.html#a1ec1018524a570304e80fb662e266f12',1,'bridges.data_src_dependent.osm.OsmData.edges(self)']]],
   ['elevation_5fserver',['elevation_server',['../namespacebridges_1_1data__src__dependent_1_1data__source.html#a5232b70889e93385e68a7cd1b296123e',1,'bridges::data_src_dependent::data_source']]]
 ];
