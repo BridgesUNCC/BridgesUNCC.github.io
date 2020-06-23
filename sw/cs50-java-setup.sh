0b3cbac80a639075a8780ce98ba7f274f5e6ec7e
Erik Saule <esaule@uncc.edu>
2020-06-23 12:23:43 -0400

    adding install script for cs50

---
+++ sw/cs50-java-setup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+wget http://bridgesuncc.github.io/sw/bridges-java-3.1.0.jar -O ~/bridges-java.jar
+
+cat >> ~/.profile <<EOF
+
+if [ "$CLASSPATH" = "" ] ;
+then
+	export CLASSPATH=.:~/bridges-java.jar
+else
+	export CLASSPATH=${CLASSPATH}:~/bridges-java.jar
+fi
+
+EOF
