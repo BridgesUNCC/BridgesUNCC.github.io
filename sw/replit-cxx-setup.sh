f0bed8fb1e29324ea2875c0eb0c5ce177944cc46
Kalpathi Subramanian <krs@uncc.edu>
2021-01-16 18:33:39 -0500

    scripts for replit, Makefile

--- sw/replit-cxx-setup.sh
+++ sw/replit-cxx-setup.sh
@@ -1,33 +1,39 @@
 #!/bin/sh
 
+REPL_PATH=$1
+echo ${REPL_PATH}
 #
 # install Curl - can only use the install-pkg script which is buggy
 #
 install-pkg libcurl4
 install-pkg libcurl4-openssl-dev
 
-#
+
+# create directory for curl includes and libraries
+mkdir ${REPL_PATH}/curl
+
+mv ~/.apt/usr/*  ${REPL_PATH}/curl
+
 # download the BRIDGES C++ client
 #
 wget http://bridgesuncc.github.io/sw/bridges-cxx-3.1.1-x86_64-linux-gnu.tgz  -O ~/bridges-cxx.tgz
 
 #
-# explode it in the home directory
+# explode it in the REPL directory
 #
-cd ${HOME}
-tar xvfz bridges-cxx.tgz
+cd ${REPL_PATH}
+tar xvfz ~/bridges-cxx.tgz
 
 #
 # need to set up environment variables that need to be appended to the .bashrc
 #
 cat >> ~/.bashrc <<EOF
 
-export BRIDGES_CXXFLAGS="-I${HOME}/bridges-cxx-3.1.1-x86_64-linux-gnu/include -I${HOME}/.apt/usr/include/x86_64-linux-gnu"
-export BRIDGES_LDFLAGS="-L${HOME}/bridges-cxx-3.1.1-x86_64-linux-gnu/lib -L{HOME}/.apt/usr/lib/x86_64-linux-gnu"
+export BRIDGES_CXXFLAGS="-I${REPL_PATH}/bridges-cxx-3.1.1-x86_64-linux-gnu/include -I${REPL_PATH}/curl/include/x86_64-linux-gnu"
+export BRIDGES_LDFLAGS="-L${REPL_PATH}/bridges-cxx-3.1.1-x86_64-linux-gnu/lib -L{HOME}/${REPL_PATH}/curl/lib/x86_64-linux-gnu"
 
 export BRIDGES_CXX=clang++
 export BRIDGES_CC=clang++
 export BRIDGES_LD=clang++
 
 EOF
-
