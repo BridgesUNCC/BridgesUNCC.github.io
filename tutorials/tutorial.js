708ee8d4ca4f34a46877822d227080157175717c
Erik Saule <esaule@uncc.edu>
2021-08-14 14:19:18 -0400

    tabbing logic corrected

--- tutorials/tutorial.js
+++ tutorials/tutorial.js
@@ -1,3 +1,4 @@
+//tabbing logic
 function displayCode(param,caller)
 {
 	var tabs = document.querySelectorAll("*.tabs > *");
@@ -13,6 +14,31 @@ function displayCode(param,caller)
 	caller.setAttribute("active","true");
 	document.getElementById(param).setAttribute("active","true");
 }
+
+function displayCodeSnippet(turnOn, category, caller)
+{
+    let tabs = document.querySelectorAll("*."+category+" > *");
+    for(let i=0;i<tabs.length;i++)
+    {
+	tabs[i].setAttribute("active", "false");
+    }
+
+    caller.setAttribute("active","true");
+    document.getElementById(turnOn).setAttribute("active","true");
+}
+
+function activateAllTabs() {
+    let tabs = document.querySelectorAll("*.tabs > *");
+    //console.log(tabs);
+    for(let i=tabs.length-1;i>=0;i--)
+    {
+	tabs[i].click();
+    }    
+
+}
+
+
+//bridges iframe
 
 setTimeout(loadFrame, 1000);
 
