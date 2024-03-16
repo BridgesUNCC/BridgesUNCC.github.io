ddc731863df0acd850de698405701149c56c5445
Erik Saule <esaule@uncc.edu>
2024-03-16 17:36:03 -0400

    separating data from formatting

---
+++ assignments/data.sh
@@ -0,0 +1,151 @@
+addgroup "CS1"
+addsubgroup "Basic API Calls"
+addassignment "27-GameGridBasic"
+addassignment "56-2DIndexing"
+
+addsubgroup "Tests and Conditions"
+addassignment "34-GameTutorials"
+addassignment "19-Bugstomp"
+
+addsubgroup "Loops"
+addassignment "25-Patterns"
+addassignment "23-MountainPaths"
+addassignment "33-AudioMixing"
+addassignment "52-AudioWave"
+addassignment "56-2DIndexing"
+addassignment "55-FrequencyPlayer"
+addassignment "68-MusicNotes-CS2"
+addassignment "65-Reddit"
+
+
+addsubgroup "Input/Output"
+addassignment "10-HurricaneTracker"
+addassignment "72-PixelDrawing"
+
+addsubgroup "1D Arrays"
+addassignment "33-AudioMixing"
+addassignment "55-FrequencyPlayer"
+addassignment "68-MusicNotes-CS2"
+addassignment "65-Reddit"
+addassignment "70-WordleBot"
+
+addsubgroup "2D Arrays"
+addassignment "6-GridSquareFill"
+addassignment "7-GridLyrics"
+addassignment "16-ImagePuzzle"
+addassignment "40-ImageProcessing"
+addassignment "56-2DIndexing"
+addassignment "52-AudioWave"
+addassignment "71-PiEstimation"
+
+
+addsubgroup "Games"
+addassignment "17-ControlsTutorial"
+addassignment "18-ControlsTutorialTwo"
+addassignment "11-2048Game"
+addassignment "13-InfiniteRunner"
+addassignment "20-Minesweeper"
+addassignment "21-RaceCar"
+addassignment "22-Snake"
+addassignment "32-TicTacToe"
+addassignment "36-Pong"
+addassignment "35-ConnectFour"
+addassignment "41-SpaceInvaders"
+addassignment "69-SnakeQueue"
+
+
+addsubgroup "Audio"
+addassignment "55-FrequencyPlayer"
+addassignment "52-AudioWave"
+addassignment "33-AudioMixing"
+addassignment "68-MusicNotes-CS2"
+
+addsubgroup "Simulation"
+addassignment "14-SpreadingFire"
+addassignment "15-FallingSand"
+
+addgroup "Data Structure"
+addsubgroup "LinkedList"
+addassignment "1-ListIMDB"
+addassignment "2-ListEQ"
+
+addsubgroup "Stacks and Queues"
+addassignment "12-AStarMaze"
+addassignment "26-TowersOfHanoi"
+addassignment "22-Snake"
+addassignment "69-SnakeQueue"
+
+addsubgroup "Recursion"
+addassignment "26-TowersOfHanoi"
+addassignment "74-Fractals"
+addassignment "75-KochCurve"
+addassignment "76-Zcurve"
+
+addsubgroup "Trees"
+addassignment "5-BST_Earthquakes"
+addassignment "8-PQBook"
+addassignment "24-ImageCompressionKdTree"
+addassignment "39-BookDistance"
+addassignment "60-Quadtree-CityData"
+
+addsubgroup "Hashing, Hash Tables, and Hash Maps"
+addassignment "8-PQBook"
+addassignment "39-BookDistance"
+
+addsubgroup "Graphs"
+addassignment "12-AStarMaze"
+addassignment "3-GraphBaconNumber"
+addassignment "30-TemporalBaconNumber"
+addassignment "4-GraphEQ"
+addassignment "9-ShortestPathOSM"
+addassignment "31-TemporalPageRank"
+addassignment "39-BookDistance"
+
+
+addgroup "Algorithms"
+addsubgroup "Big Oh"
+addassignment "28-BigOhMatters"
+
+addsubgroup "Sorting"
+addassignment "29-SortingBenchmark"
+
+addsubgroup "Brute Force"
+addassignment "64-Voronoi_Diagram"
+
+addsubgroup "Divide and Conquer"
+addassignment "59-ConvexHull_CityData"
+
+addsubgroup "Search space"
+addassignment "70-WordleBot"
+addassignment "12-AStarMaze"
+addassignment "9-ShortestPathOSM"
+addassignment "23-MountainPaths"
+
+addsubgroup "Complexity of Graph Problems"
+addassignment "9-ShortestPathOSM"
+addassignment "30-TemporalBaconNumber"
+addassignment "31-TemporalPageRank"
+
+addsubgroup "Spanning Tree"
+addassignment "44-MST-Cities"
+
+addsubgroup "Spatial Problems"
+addassignment "43-SpatialIndexing"
+addassignment "59-ConvexHull_CityData"
+addassignment "60-Quadtree-CityData"
+
+addsubgroup "Greedy Algorithms"
+addassignment "23-MountainPaths"
+addassignment "9-ShortestPathOSM"
+addassignment "38-GeologicalSurvey"
+addassignment "44-MST-Cities"
+
+addsubgroup "Dynamic Programing"
+addassignment "23-MountainPaths"
+addassignment "9-ShortestPathOSM"
+
+addsubgroup "NP-Complete problems"
+addassignment "38-GeologicalSurvey"
+
+addsubgroup "Multi objective optimization"
+addassignment "23-MountainPaths"
